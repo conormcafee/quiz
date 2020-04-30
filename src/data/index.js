6f95684a22fa9eefc13f1e0ad8d1c8d1faa2fd53
Conor McAfee <hello@conormcafee.co.uk>
2020-04-30 19:03:20 +0100

    Removed data from gitignore

---
+++ src/data/index.js
@@ -0,0 +1,59 @@
+import Char01 from "./characters/comic-book-guy.jpg";
+import Char02 from "./characters/fat-tony.jpg";
+import Char03 from "./characters/granpa.png";
+import Char04 from "./characters/luann.jpg";
+import Char05 from "./characters/nelson.jpg";
+import Char06 from "./characters/moe.jpg";
+import Char07 from "./characters/troy.png";
+import Char08 from "./characters/martin.jpeg";
+
+export const characters = [
+  {
+    image: Char01,
+    scale: 3,
+    rotate: true,
+    position: `3px,15px`,
+  },
+  {
+    image: Char02,
+    scale: 3,
+    rotate: true,
+    position: `18px,29px`,
+  },
+  {
+    image: Char03,
+    scale: 3,
+    rotate: true,
+    position: `-36px,34px`,
+  },
+  {
+    image: Char04,
+    scale: 3,
+    rotate: true,
+    position: `31px,-91px`,
+  },
+  {
+    image: Char05,
+    scale: 3,
+    rotate: true,
+    position: `66px,106px`,
+  },
+  {
+    image: Char06,
+    scale: 2,
+    rotate: true,
+    position: `6px,60px`,
+  },
+  {
+    image: Char07,
+    scale: 3,
+    rotate: true,
+    position: `101px,69px`,
+  },
+  {
+    image: Char08,
+    scale: 3,
+    rotate: true,
+    position: `130px,-102px`,
+  },
+];
